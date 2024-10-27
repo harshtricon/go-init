09d5c388cc0c21d0783e0273c8cd7dc60131fbb2
google authorization <harsh.kumar@triconinfotech.com>
2024-10-27 16:53:01 +0530

    added divide function

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
-
 import "fmt"
-
-func main() {
-	fmt.Println("hello")
+func main(){
+var printValue string = "Hello World"
+printMe (printValue)
+var numerator int = 11
+var denominator int= 2
+var result int = intDivision (numerator, denominator)
+fmt.Println(result)
+}
+func printMe (printValue string) {
+fmt.Println(printValue)
+}
+func intDivision (numerator int, denominator int) int {
+var result int = numerator/denominator
+return result
 }
